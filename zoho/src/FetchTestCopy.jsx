3d006d68de6bd95ea2ed3f6362301e70084b786f
Symon <simon16143@gmail.com>
2023-01-06 13:37:38 -0500

    updating information 06-01-2023

--- zoho/src/FetchTestCopy.jsx
+++ zoho/src/FetchTestCopy.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
@@ -11,8 +11,6 @@ import {AppBar, Avatar, Badge, InputBase, Menu, MenuItem, styled, Toolbar} from 
 import MailIcon from '@mui/icons-material/Mail';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 
-
-
 //Estilos para el header
 const StyledToolbar = styled(Toolbar)({
     display:"flex",
@@ -20,7 +18,6 @@ const StyledToolbar = styled(Toolbar)({
 const Search = styled("div")(({theme})=>({
     backgroundColor: "white",
     padding: "0 10px",
-    justifyContent:'center',
     borderRadius:theme.shape.borderRadius,
     width:"30%"
 }))
@@ -32,21 +29,37 @@ const Icons = styled(Box)(({theme})=>({
 }))
 
 
-const FetchTest = (props) => {
-  //State para el filtro de la consulta
+const FetchTestCopy = () => {
+  const [data, setData] = useState([]);
   const [query, setQuery] = useState("");
-  //State para el modal del avatar
   const[open,setOpen] = useState(false);
-  
+  const urlPost = "https://accounts.zoho.eu/oauth/v2/token?refresh_token=1000.2e85c3c9c4d568e485f620b35403bc6f.d5652d764a999e4c1763325058efb2a2&client_id=1000.L10967VQ19WMBCP5CMHPC9DOJCAI9J&client_secret=eeba586124ab65019a8c46d74034ac5beda988943b&grant_type=refresh_token";
+  const urlGet = "Product-Catalog/report/Product_Details";
+  useEffect(()=>{ 
+    const peticion = fetch(urlPost,{
+      method: 'POST',
+    });
+    peticion
+    .then((ans)=>{return ans.json()})
+    .then((resp)=>{
+      const reslt = resp.access_token;
+      console.log(reslt)
+      return fetch(urlGet,{
+        method: "GET",
+        headers:{'Authorization':`Zoho-oauthtoken ${reslt}` }})})
+        .then((answer) => { return answer.json() })
+        .then((resp) => {
+          const result = resp.data
+          setData(result)})
+   
+  },[])   
 return (
     <Box bgcolor="white" flex={4} p={2} align="center" > 
         <AppBar position="sticky">
-       
         <StyledToolbar>
-          <Typography variant="h5" >
-           Catálogo
-          </Typography>
-  
+            <Typography variant="h5">
+                Hostienda
+            </Typography>
 
         <Search>
             <InputBase fullWidth={true} placeholder='Buscar...' type="text"className='search'
@@ -60,10 +73,8 @@ return (
             <Badge badgeContent={4} color="error">
              <NotificationsIcon/>
             </Badge>
-            
-            <Avatar  sx={{width:30, height:30}} alt="Symon"
-            onClick={e=>setOpen(true)}/>   
-  
+            <Avatar sx={{width:30, height:30}} alt="Symon" src=""
+            onClick={e=>setOpen(true)}/>      
         </Icons> 
 
     </StyledToolbar>
@@ -87,35 +98,32 @@ return (
    
     <div align="center">
     <Card sx={{ maxWidth: 345 }}> 
-      {/*Filtro de categorías*/}
-      {props.data.filter((item)=>item.Category.display_value.toLowerCase().includes(query.toLowerCase()))
-      .map((product)=>(
-        /*Key obligatorio*/ 
-      <p key={product.Product_ID}> 
-        <Typography fontSize={25} fontFamily='arial' color={"#52796f"}>{product.Product_Name}</Typography>
-      <img src= {"https://creator.zoho.eu" + product.Product_Images[0].display_value}></img><br/>
-      <CardContent>
-        <Typography textAlign={"center"}>{product.Product_Description}</Typography>
-        <Typography> Precio: € {product.Product_Value}<br/></Typography>
-        <Typography> Categoría: {product.Category.display_value}</Typography>
-        <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-      </CardActions>
-      </CardContent>   
-      </p>
-        ))}
-
-
-    </Card>
+     {data.filter((item)=>item.Category.display_value.toLowerCase().includes(query.toLowerCase()))
+     .map((product)=>(
+      /*Key obligatorio*/ 
+     <p key={product.Product_ID}> 
+      <Typography fontSize={25} fontFamily='arial' color={"#52796f"}>{product.Product_Name}</Typography>
+     <img src= {"https://creator.zoho.eu" + product.Product_Images[0].display_value}></img><br/>
+     <CardContent>
+      <Typography textAlign={"center"}>{product.Product_Description}</Typography>
+      <Typography> Precio: € {product.Product_Value}<br/></Typography>
+      <Typography> Categoría: {product.Category.display_value}</Typography>
+      <CardActions disableSpacing>
+      <IconButton aria-label="add to favorites">
+        <FavoriteIcon />
+      </IconButton>
+      <IconButton aria-label="share">
+        <ShareIcon />
+      </IconButton>
+    </CardActions>
+    </CardContent>   
+     </p>
+       ))}
+  </Card>
   <br/>
   </div>
   </Box>
 );
   }
 
-export default FetchTest
+export default FetchTestCopy
