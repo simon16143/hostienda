511b0a633867cf911d10bbf97b0186995d77f149
Symon <simon16143@gmail.com>
2023-01-11 20:19:50 -0500

    fixing cors errors

--- zoho/src/App.jsx
+++ zoho/src/App.jsx
@@ -31,25 +31,12 @@ function App() {
   const [active, setActive] = useState("");
    //Peticiones para el token y el archivo json
   const [data, setData] = useState([]);
-  const urlPost = "https://accounts.zoho.eu/oauth/v2/token?refresh_token=1000.3dbdad6937dc0800c4dcc662cd14d173.86efb18e337989bebb3ff4c05582c94c&client_id=1000.NQL17JHK3Y62Y178TO0E3FQC6MBQJV&client_secret=5d04ad135862e7313377484af55efa1f41c1f49a39&grant_type=refresh_token";
-  const urlGet = "https://creator.zoho.eu/api/v2/hostienda1/Product-Catalog/report/Product_Details";
   useEffect(()=>{ 
-    const peticion = fetch(urlPost,{
-      method: 'POST',
-    });
+    const peticion = fetch("http://localhost:4000")
     peticion
-    .then((ans)=>{return ans.json()})
-    .then((resp)=>{
-      const reslt = resp.access_token;
-      return fetch(urlGet,{
-        method: "GET",
-        headers:{
-            'Authorization':`Zoho-oauthtoken ${reslt}`,
-           }}
-         )})
-        .then((answer) => { return answer.json() })
-        .then((resp) => {
-          const result = resp.data
+    .then(res => res.json())
+    .then(data =>{
+          const result = data.data
           setData(result)
           setFiltered(result)
         }) 
