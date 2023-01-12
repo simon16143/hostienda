b20c1c11ea3090e59a6ca3f1270fe232cb9979ae
Symon <simon16143@gmail.com>
2023-01-11 20:41:28 -0500

    fixing route2

--- zoho/src/App.jsx
+++ zoho/src/App.jsx
@@ -32,7 +32,7 @@ function App() {
    //Peticiones para el token y el archivo json
   const [data, setData] = useState([]);
   useEffect(()=>{ 
-    const peticion = fetch("https://hostienda-l71o.vercel.app:4000")
+    const peticion = fetch("https://hostienda-l71o.vercel.app")
     peticion
     .then(res => res.json())
     .then(data =>{
